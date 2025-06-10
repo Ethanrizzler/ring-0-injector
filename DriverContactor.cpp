97bb82bae0e08c0de68000c815d7bc755a3ee43e
Ethan <ethanondablock786@gmail.com>
2025-06-10 19:59:44 +0200

    Update DriverContactor.cpp

--- DriverContactor.cpp
+++ DriverContactor.cpp
@@ -1,1 +1,7 @@
-// nothing in here
+here you make the main stuff
+
+for RobloxPlayerBeta.exe
+
+Send the pid and wich process to inject to To the driver
+
+when to write and patch!    make a mapping logic that handles with the raw.h your dll image !
